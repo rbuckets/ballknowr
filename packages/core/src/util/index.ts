7a81a699a6e1cd22f68fd1923045748faf94bc24
rbuckets <wangrayray@gmail.com>
2024-10-22 20:17:13 -0700

    Enabling CORS

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -23,6 +23,10 @@ export module Util {
       return {
         body,
         statusCode,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
       };
     };
   }
